635df88e19d24113535518370a8329c2ec4ac41b
shabdulhadi <ah5672758@gmail.com>
2024-12-28 13:06:54 +0500

    Add files via upload
    speed

---
+++ speed.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main() {
+	cout << "enter distance";
+	int distance;
+	cin >> distance;
+	cout << "enter time";
+	int time;
+	cin >> time;
+	cout << "the speed is" << distance / time;
+	return 0;
+
+
+	
+}
